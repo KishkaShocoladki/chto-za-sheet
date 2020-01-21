33a5513144ce9e286f01fe0ee6d47db7f75ad5ec
Mike Milligram <55143644+KishkaShocoladki@users.noreply.github.com>
2020-01-21 18:03:36 +0400

    ХачюдДомой

---
+++ WindowsFormsApp2/Persons.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Persons
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Persons));
+            this.button1 = new System.Windows.Forms.Button();
+            this.persons1 = new System.Windows.Forms.PictureBox();
+            this.persons2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.persons3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.persons4 = new System.Windows.Forms.PictureBox();
+            this.persons5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.persons1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Лань Линь Цзинь";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // persons1
+            // 
+            this.persons1.Image = ((System.Drawing.Image)(resources.GetObject("persons1.Image")));
+            this.persons1.Location = new System.Drawing.Point(26, 13);
+            this.persons1.Name = "persons1";
+            this.persons1.Size = new System.Drawing.Size(158, 157);
+            this.persons1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.persons1.TabIndex = 1;
+            this.persons1.TabStop = false;
+            this.persons1.Click += new System.EventHandler(this.persons1_Click);
+            // 
+            // persons2
+            // 
+            this.persons2.Image = ((System.Drawing.Image)(resources.GetObject("persons2.Image")));
+            this.persons2.Location = new System.Drawing.Point(323, 12);
+            this.persons2.Name = "persons2";
+            this.persons2.Size = new System.Drawing.Size(154, 157);
+            this.persons2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.persons2.TabIndex = 2;
+            this.persons2.TabStop = false;
+            this.persons2.Click += new System.EventHandler(this.persons2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Юн Мэн Цзян";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // persons3
+            // 
+            this.persons3.Image = ((System.Drawing.Image)(resources.GetObject("persons3.Image")));
+            this.persons3.Location = new System.Drawing.Point(616, 12);
+            this.persons3.Name = "persons3";
+            this.persons3.Size = new System.Drawing.Size(154, 157);
+            this.persons3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.persons3.TabIndex = 4;
+            this.persons3.TabStop = false;
+            this.persons3.Click += new System.EventHandler(this.persons3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(616, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 31);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Гу Су Лань";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(176, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 31);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Ци Шань Вэнь";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(467, 394);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 31);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Цин Хэ Нэ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // persons4
+            // 
+            this.persons4.Image = ((System.Drawing.Image)(resources.GetObject("persons4.Image")));
+            this.persons4.Location = new System.Drawing.Point(176, 231);
+            this.persons4.Name = "persons4";
+            this.persons4.Size = new System.Drawing.Size(158, 157);
+            this.persons4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.persons4.TabIndex = 8;
+            this.persons4.TabStop = false;
+            // 
+            // persons5
+            // 
+            this.persons5.Image = ((System.Drawing.Image)(resources.GetObject("persons5.Image")));
+            this.persons5.Location = new System.Drawing.Point(467, 231);
+            this.persons5.Name = "persons5";
+            this.persons5.Size = new System.Drawing.Size(158, 157);
+            this.persons5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.persons5.TabIndex = 9;
+            this.persons5.TabStop = false;
+            // 
+            // Persons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.persons5);
+            this.Controls.Add(this.persons4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.persons3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.persons2);
+            this.Controls.Add(this.persons1);
+            this.Controls.Add(this.button1);
+            this.Name = "Persons";
+            this.Text = "Пони";
+            ((System.ComponentModel.ISupportInitialize)(this.persons1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persons5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox persons1;
+        private System.Windows.Forms.PictureBox persons2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox persons3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox persons4;
+        private System.Windows.Forms.PictureBox persons5;
+    }
+}
